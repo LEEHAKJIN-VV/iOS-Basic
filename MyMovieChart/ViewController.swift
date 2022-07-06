80345913464baa4308a319213b5a26cea9af6527
LEEHAKJIN_VV <hagjini0@gmail.com>
2022-07-06 18:42:02 +0900

    Initial Commit

---
+++ MyMovieChart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by 이학진 on 2022/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
